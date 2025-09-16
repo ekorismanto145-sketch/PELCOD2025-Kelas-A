b428583fec34ff521272db835ef55ac65ce2965f
ekorismanto145 <ekorismanto145@gmail.com>
2025-09-16 19:30:20 +0700

    Tugas 1 PELCOD

---
+++ soal1.py
@@ -0,0 +1,12 @@
+# soal no.1
+
+durasi_per_episode = 35
+jumlah_episode = 10
+
+total_menit = durasi_per_episode * jumlah_episode
+jam = total_menit // 60
+menit_sisa = total_menit % 60
+print("Total nonton:", total_menit, "menit")
+print("Atau =", jam, "jam", menit_sisa, "menit")
+
+
